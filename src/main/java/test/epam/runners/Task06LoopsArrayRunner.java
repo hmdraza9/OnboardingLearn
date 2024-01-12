6997334295fafb5eaf6ca434cb96d3157e7374c8
Hamid Raza <hamid_raza@epam.com>
2024-01-12 22:16:21 +0530

    Task06LoopsArray complete

--- src/main/java/test/epam/runners/Task06LoopsArrayRunner.java
+++ src/main/java/test/epam/runners/Task06LoopsArrayRunner.java
@@ -6,11 +6,14 @@ public class Task06LoopsArrayRunner {
 
 	public static void main(String[] args) {
 
-//		Task06LoopsArray.maxSpan(new int[] { 1, 2, 1, 1, 3 });
-//		Task06LoopsArray.maxSpan(new int[] { 1, 4, 2, 1, 4, 1, 4 });
-//		Task06LoopsArray.maxSpan(new int[] { 1, 4, 2, 1, 4, 4, 4 });
-		System.out.println("Can be balanced? "+Task06LoopsArray.canBalance(new int[] { 1, 2, 3, 4, 5, 6, 7 }));
-		System.out.println("Can be balanced? "+Task06LoopsArray.canBalance(new int[] {1, 1, 1, 1, 2, 1 ,1}));
+		Task06LoopsArray.maxSpan(new int[] { 1, 2, 1, 1, 3 });
+		Task06LoopsArray.maxSpan(new int[] { 1, 4, 2, 1, 4, 1, 4 });
+		Task06LoopsArray.maxSpan(new int[] { 1, 4, 2, 1, 4, 4, 4 });
+		System.out.println("Can be balanced? " + Task06LoopsArray.canBalance(new int[] { 1, 1, 1, 1, 2, 1, 1 }));
+		System.out.println("Lumps? " + Task06LoopsArray.countClumps(new int[] { 1, 2, 2, 3, 4, 4 }));
+		System.out.println("Max mirror size: " + Task06LoopsArray.maxMirror(new int[] { 1, 2, 3, 8, 9, 3, 2, 1 }));
+		System.out.println("Linear In: "
+				+ (Task06LoopsArray.linearIn(new int[] { -1, 0, 3, 3, 3, 10, 12 }, new int[] { -1, 0, 3, 12 })));
 
 	}
 
--- src/main/java/test/epam/task06/Task06LoopsArray.java
+++ src/main/java/test/epam/task06/Task06LoopsArray.java
@@ -12,12 +12,7 @@ public class Task06LoopsArray {
 				if (nums[i] == nums[j]) {
 					temp = j - i + 1;
 					span = Math.max(span, temp);
-//					System.out.println("nums[i]: " + nums[i] + " nums[j]: " + nums[j] + " Local span: " + temp
-//							+ "; Max Span: " + span);
 				}
-//				else
-//					System.out.println("nums[i]: " + nums[i] + " nums[j]: " + nums[j] + " Local span: " + temp
-//							+ "; Max Span: " + span);
 
 			}
 		}
@@ -60,5 +55,89 @@ public class Task06LoopsArray {
 		}
 		return isBalanced;
 
+	}
+
+	public static int countClumps(int[] numArray) {
+
+		int lumpCount = 0;
+		int prevInt = -1;
+
+		for (int i = 0; i < numArray.length - 1; i++) {
+			int temp = numArray[i + 1];
+			if (numArray[i] == temp && numArray[i] != prevInt) {
+
+				System.out.println("Lump detected: " + numArray[i] + " : " + temp);
+				lumpCount++;
+				prevInt = numArray[i];
+
+			}
+
+			prevInt = numArray[i];
+		}
+		return lumpCount;
+	}
+
+	public static int maxMirror(int[] nums) {
+
+		int len = nums.length;
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < len; i++) {
+			count = 0;
+			for (int j = len - 1; i + count < len && j > -1; j--) {
+				if (nums[i + count] == nums[j]) {
+					count++;
+				} else {
+					if (count > 0) {
+						max = Math.max(count, max);
+						count = 0;
+					}
+				}
+			}
+			max = Math.max(count, max);
+		}
+		return max;
+	}
+
+	public static boolean linearIn(int[] numArray, int[] subNumArray) {
+
+		int[] tempArray = new int[subNumArray.length];
+
+		boolean isLinearIn = true;
+
+		for (int i = 0; i < numArray.length - tempArray.length + 1; i++) {
+			for (int m = 0; m < (tempArray.length); m++) {
+				tempArray[m] = numArray[m + i];
+			}
+//			System.out.println("i= " + i + " tempArray size: " + tempArray.length);
+			System.out.print("Sub array : ");
+			printArray(subNumArray);
+			System.out.print("Temp array: ");
+			printArray(tempArray);
+			System.out.println("Is match? " + (tempArray == subNumArray));
+
+			for (int k = 0; k < tempArray.length; k++) {
+
+				isLinearIn = tempArray[k] == subNumArray[k];
+
+				if (!isLinearIn)
+					break;
+
+			}
+			System.out.println("Is match? " + isLinearIn);
+			if (isLinearIn)
+				break;
+
+		}
+		return isLinearIn;
+
+	}
+
+	private static void printArray(int[] arrAy) {
+//		System.out.println("Printing Array:");
+		for (int t : arrAy) {
+			System.out.print(t + " ");
+		}
+		System.out.println("");
 	}
 }
